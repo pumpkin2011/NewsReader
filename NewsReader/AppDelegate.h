8e8c9162cae1e5ed1d88d7669d74baf4ac8874a3
pumpkin <zhenhuanlee@outlook.com>
2017-06-24 10:23:08 +0800

    Initial Commit

---
+++ NewsReader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewsReader
+//
+//  Created by Aico on 6/24/17.
+//  Copyright © 2017 lee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
